8162292b3aef1f31f58198f9e0114ccfe7248dbc
Michael Diener <dm.menthos@gmail.com>
2013-04-23 21:42:15 -0700

    updated windows setup
    Updated the setup for windows

--- setup_win.py
+++ setup_win.py
@@ -5,9 +5,20 @@ import py2exe
 import os
 
 sys.path.append(os.path.join(os.getcwd(), 'grace'))
-sys.path.append(os.path.join('C:\\Python27\\Scripts'))
+sys.path.append('C:\\Python27\\Scripts')
+sys.path.append('C:\\Python27\\Lib\\site-packages')
 sys.path.append('C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VC\\redist\\x86\\Microsoft.VC110.CRT')
-data_files = [('Microsoft.VC110.CRT', glob(r'C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\redist\x86\Microsoft.VC110.CRT\*.*'))]
+data_files = [
+    ('Microsoft.VC110.CRT', glob(r'C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\redist\x86\Microsoft.VC110.CRT\*.*')),
+    ('plugins', glob(os.path.join('grace', 'plugins') + '\*.py'))
+]
+
+previous = ''
+for root, dirs, files in os.walk(os.path.join('grace', 'skeletons')):
+    for filename in files:
+        if previous != root:
+            data_files.append((root[6:], glob(root + '\*.*')))
+            previous = root
 
 setup(
     name='grace',
@@ -16,7 +27,14 @@ setup(
     author_email='dm.menthos@gmail.com',
     url='https://github.com/mdiener/grace',
     version='0.1',
-    package=['grace'],
+    packages=['grace'],
     data_files=data_files,
-    console=['grace/main.py']
+    console=['grace/grace.py'],
+    options={
+        'py2exe': {
+            'packages': ['grace'],
+            'bundle_files': True,
+            'includes': ['grace.build', 'grace.deploy', 'grace.error', 'grace.create', 'grace.task', 'grace.testit', 'grace.zipit', 'sass', 'plistlib']
+        }
+    }
 )
