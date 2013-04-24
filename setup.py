fcd9e0cd295b7bab8d391319b9f8e06a54acc7ef
Michael Diener <dm.menthos@gmail.com>
2013-04-23 22:45:13 -0700

    updated setup for linux (still needs testing ...)

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import sys
+import os
 from distutils.core import setup
+
+sys.path.append(os.path.join(os.getcwd(), 'grace'))
+data_files = [
+    ('plugins', glob(os.path.join('grace', 'plugins') + '\*.py'))
+]
+
+previous = ''
+for root, dirs, files in os.walk(os.path.join('grace', 'skeletons')):
+    for filename in files:
+        if previous != root:
+            data_files.append((root[6:], glob(root + '\*.*')))
+            previous = root
 
 setup(
     name='grace',
@@ -7,5 +21,6 @@ setup(
     author_email='dm.menthos@gmail.com',
     url='https://github.com/mdiener/grace',
     version='0.1',
-    packages=['grace']
+    packages=['grace'],
+    data_files=data_files
 )
