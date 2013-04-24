aba594aa30e593dccd9be86de36ec4ac1004b252
Michael Diener <dm.menthos@gmail.com>
2013-04-23 18:32:54 -0700

    updated setup.py for windows

--- setup_win.py
+++ setup_win.py
@@ -2,7 +2,10 @@ from glob import glob
 from distutils.core import setup
 import sys
 import py2exe
+import os
 
+sys.path.append(os.path.join(os.getcwd(), 'grace'))
+sys.path.append(os.path.join('C:\\Python27\\Scripts'))
 sys.path.append('C:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\VC\\redist\\x86\\Microsoft.VC110.CRT')
 data_files = [('Microsoft.VC110.CRT', glob(r'C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\redist\x86\Microsoft.VC110.CRT\*.*'))]
 
@@ -13,6 +16,7 @@ setup(
     author_email='dm.menthos@gmail.com',
     url='https://github.com/mdiener/grace',
     version='0.1',
-    packages=['grace'],
-    data_files=data_files
+    package=['grace'],
+    data_files=data_files,
+    console=['grace/main.py']
 )
