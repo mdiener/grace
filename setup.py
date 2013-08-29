5fb72d3dc4d4016dc61574159c623b900bf76560
Michael Diener <dm.menthos@gmail.com>
2013-08-28 21:04:44 -0700

    added pyjsdoc to required install packages

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license='LICENSE.txt',
     scripts=['bin/grace'],
     packages=['grace'],
-    install_requires=['libsass', 'argparse', 'setuptools', 'slimit', 'cssmin'],
+    install_requires=['libsass', 'argparse', 'setuptools', 'slimit', 'cssmin', 'pyjsdoc'],
     package_data=package_data,
     keywords='toolchain javascript dizmo js buildtool',
     long_description=open('README.txt').read(),
