84a760eaf4a2426e5af20425c2265de62aa2c823
Michael Diener <dm.menthos@gmail.com>
2014-03-04 01:21:26 -0800

    Grace Management
    First try on a management file

---
+++ grace/management.py
@@ -0,0 +1,7 @@
+import sys
+from grace.task import Task
+from grace.error import FileNotFoundError, WrongFormatError, MissingKeyError, CreateFolderError, FolderNotFoundError, FileNotWritableError, RemoveFolderError, RemoveFileError, FolderAlreadyExistsError, SassError, UnknownCommandError
+
+
+def execute(args):
+
