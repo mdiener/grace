06d8950c37ea7d93bbd9f636576407eb5b336a78
Michael Diener <dm.menthos@gmail.com>
2014-04-10 22:05:30 -0700

    Skeleton
    * added more sophisticated test setup
    
    Signed-off-by: Michael Diener <dm.menthos@gmail.com>

---
+++ grace/skeleton/default/test/javascript/test_cases.js
@@ -0,0 +1,6 @@
+module('my first module');
+test('test case one', function(assert) {
+    // do testing here.
+    // refer to the QUnit documentation for more information
+    // https://qunitjs.com/
+});
