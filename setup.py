a77da71a07e6623c372202a7f1feedccb35c0cf6
Michael Diener <dm.menthos@gmail.com>
2014-07-24 12:46:42 -0700

    Switched To GPL as a License
    Signed-off-by: Michael Diener <dm.menthos@gmail.com>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author='Michael Diener',
     author_email='michael@webdiener.ch',
     url='https://github.com/mdiener/grace',
-    version='0.2.14',
+    version='0.3.0',
     license='LICENSE.txt',
     scripts=['bin/grace'],
     packages=['grace'],
