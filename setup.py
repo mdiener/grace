71e493a9e5374967c5f481e2839c94bb14cbe3cd
Michael Diener <dm.menthos@gmail.com>
2014-07-28 13:01:18 -0700

    increased version
    Signed-off-by: Michael Diener <dm.menthos@gmail.com>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author='Michael Diener',
     author_email='michael@webdiener.ch',
     url='https://github.com/mdiener/grace',
-    version='0.3.0',
+    version='0.3.1',
     license='LICENSE.txt',
     scripts=['bin/grace'],
     packages=['grace'],
