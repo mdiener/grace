c79126e0d8e204a329471bf1b5093da15d749ca5
Michael Diener <michael@webdiener.ch>
2015-05-28 16:21:59 -0700

    increased version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author='Michael Diener',
     author_email='michael@webdiener.ch',
     url='https://github.com/mdiener/grace',
-    version='0.4.5',
+    version='0.4.6',
     license='LICENSE.txt',
     scripts=['bin/grace'],
     packages=['grace'],
